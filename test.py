6b717dd06b90fc3f318f2a299181d62b59182adf
Robin Koetsier <45625446+RobinKoetsier@users.noreply.github.com>
2021-08-17 13:21:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,7 @@
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-
-url = 'https://github.com/'
-
-options = Options()
-options.add_argument("--headless")
-options.add_argument("window-size=1400,1500")
-
+from selenium.webdriver.chrome.options import Optionsoptions = Options()
+options.headless = True
 driver = webdriver.Chrome(options=options)
-
-# Navigate to github.com
-driver.get(url)
-
-# Extract the top heading from github.com
-text = driver.find_element_by_class_name('h000-mktg').text
-
-print(text)
+driver.get("https://www.youtube.com/")
+element_text = driver.find_element_by_id("title").text
+print(element_text)
